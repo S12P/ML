299736dfb801bbca383a87c46806cc5ecd15adf0
Stéphane <stephane.pouget@ens-lyon.fr>
2018-04-13 11:13:52 +0200

    output

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import keras
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Add
 from keras.layers.recurrent import SimpleRNN
 from keras.models import Model
 
@@ -14,16 +14,10 @@ h3 = Dense(64, activation='relu')(h2)
 lb = SimpleRNN(64, go_backwards = True)(h3)
 lf = SimpleRNN(64)(h3)
 
-h4 = keras.layers.Add()([lb,lf]) #merge
+h4 = Add()([lb,lf]) #merge
 
 h5 = Dense(64, activation='relu')(h4)
 
-#output = ?
+outputs =Dense(10, activation = 'softmax')(h5)
 
-
-def h_6(k, l, t, W, b, input):
-    """k est la colonne"""
-    den = 0
-    for k in range(len(W[6])):
-        den += np.exp(np.dot(W[6].transpose()[k]*h(5, t, W, b, input)) + b[6][k])
-    return( (np.exp(np.dot(W[6].transpose()[k]*h(5, t, W, b, input)) + b[6][k])) / den)
+model = keras.models.Model(inputs = inputs, outputs = outputs)
