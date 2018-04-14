7d213e9d962bbad3b0219e85a281bfec3c03928b
Stéphane <stephane.pouget@ens-lyon.fr>
2018-04-14 15:35:29 +0200

    saves the weights of the model

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
 import tensorflow as tf
 import keras
+from sound import sound
 from keras.layers import Input, Dense, Add
 from keras.layers.recurrent import GRU
 from keras.models import Model
+
+model.load_weights(models/)
 
 inputs = Input(shape=(784,5,))
 
@@ -21,3 +24,6 @@ h5 = Dense(64, activation='relu')(h4)
 outputs =Dense(10, activation = 'softmax')(h5)
 
 model = keras.models.Model(inputs = inputs, outputs = outputs)
+
+
+model.save_weights(models/)
