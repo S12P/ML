fadc7428f5a7c7cf6cb63f1b67da3b3ce74aea16
Stéphane <stephane.pouget@ens-lyon.fr>
2018-04-14 17:34:39 +0200

    m

--- sound.py
+++ sound.py
@@ -2,26 +2,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile
+
 import numpy as np
 import wave
 
-def sound(file_name):
-    """
-    input : wav file
 
-    output : (audio : n frames of audio ) (framerate : sampling frequency) (duration : duration in seconde)
-
-    """
-    file = wave.open(file_name, 'r')
-
-    nframes = file.getnframes()
-    framerate = file.getframerate()
-    duration = nframes / framerate
-
-    audio = np.frombuffer(file.readframes(-1), dtype=np.int16)
-
-    file.close()
-
-    return(audio, framerate, duration)
-
-print(sound("yes.wav"))
+def spectogram(file):
+    window_size=20
+    step_size=10
+    sample_rate, audio = wavfile.read(file)
+    nperseg = window_size * sample_rate / 1e3
+    noverlap = step_size * sample_rate / 1e3
+    _, _, spectogram = signal.spectrogram(audio, fs=sample_rate, window='hann', nperseg=int(nperseg), noverlap=int(noverlap), detrend=False)
+    return(spectogram.T)
