927b5cd7398a03c32274bd192c04a2b351805c68
Vincent Rébiscoul <vincent.rebiscoul@ens-lyon.fr>
2018-04-24 19:37:48 +0200

    change the model, add lambda layer & input layers
    keras does not handle more than 2 arguments for the loss function, thus
    the computation of the loss is done in the lambda layer which takes as
    inputs the predictions, the labels, input_length and label_length

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import numpy as np
 import tensorflow as tf
 import keras
 import sound
-from keras.layers import Input, Dense, Add
+from keras.layers import Input, Dense, Add, Lambda
 from keras.layers.recurrent import GRU
 from keras.models import Model
 import keras.backend as K
 from keras.optimizers import SGD
 
 
-def ctc_loss(y_true, y_pred):
-    input_length = np.array([[2], [2]]) # a changer
-    label_length = np.array([[2], [2]]) # a changer
+def ctc_loss_lambda(args):
+    y_pred, y_true, input_length, label_length = args
 
-    input_length = tf.convert_to_tensor(input_length, dtype=tf.float32)
-    label_length = tf.convert_to_tensor(label_length, dtype=tf.float32)
-    return K.mean(K.ctc_batch_cost(tf.squeeze(y_true), y_pred, input_length, label_length), axis=-1)
+    return K.mean(K.ctc_batch_cost(y_true, y_pred, input_length, label_length), axis=-1)
+
+def ctc(y_true, y_pred):
+    return y_pred
 
 
 # model.load_weights('models/')
@@ -25,6 +25,9 @@ MAX_TIME_FRAMES = 2
 MAX_LABEL_SIZE = 100
 
 inputs = Input(shape=(MAX_TIME_FRAMES, NB_FREQUENCIES))
+labels = Input(shape=[None])
+input_length = Input(shape=[1,])
+label_length = Input(shape=[1,])
 
 h1 = Dense(64, activation='relu')(inputs)
 h2 = Dense(64, activation='relu')(h1)
@@ -36,22 +39,25 @@ lf = GRU(64, return_sequences=True)(h3)
 h4 = Add()([lb,lf]) #merge
 
 h5 = Dense(64, activation='relu')(h4)
+h6 = Dense(29, activation='softmax')(h5)
 
-outputs = Dense(29, activation = 'softmax')(h5)
+loss_out = Lambda(ctc_loss_lambda, output_shape=(1, ))([h6, labels, input_length, label_length])
 
-model = keras.models.Model(inputs=inputs, outputs=outputs)
+model = keras.models.Model(inputs=[inputs, labels, input_length, label_length], outputs=loss_out)
 model.summary()
 
 sgd = SGD(nesterov=True)
-model.compile(loss=ctc_loss, metrics=['accuracy'], optimizer=sgd)
+model.compile(loss=ctc, metrics=['accuracy'], optimizer=sgd)
 
 ex1 = np.random.random_sample((2,2))
 ex2 = np.random.random_sample((2,2))
 batch = np.array([ex1, ex2])
 
-truth = np.array([[[0, 1]], [[0,1]]])
+lab = np.array([[1,2],[1,2]])
+input_len = np.array([2, 2])
+lab_len = np.array([2, 2])
 
-model.fit(batch, truth, epochs=1)
+model.fit([batch, lab, input_len, lab_len], lab)
 
 
 # model.save_weights('models/')
