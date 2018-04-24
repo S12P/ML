e2e6f83a0d3061da8e36441c5e6c463610917ae6
Vincent Rébiscoul <vincent.rebiscoul@ens-lyon.fr>
2018-04-24 19:12:37 +0200

    making tests

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ from keras.optimizers import SGD
 
 
 def ctc_loss(y_true, y_pred):
-    input_length = np.zeros((5, 1)) # a changer
-    label_length = np.zeros((5, 1)) # a changer
+    input_length = np.array([[2], [2]]) # a changer
+    label_length = np.array([[2], [2]]) # a changer
 
     input_length = tf.convert_to_tensor(input_length, dtype=tf.float32)
     label_length = tf.convert_to_tensor(label_length, dtype=tf.float32)
-    return K.mean(K.ctc_batch_cost(K.batch_flatten(y_true), y_pred, input_length, label_length), axis=-1)
+    return K.mean(K.ctc_batch_cost(tf.squeeze(y_true), y_pred, input_length, label_length), axis=-1)
 
 
 # model.load_weights('models/')
 
-NB_FREQUENCIES = 9000
-MAX_TIME_FRAMES = 500
+NB_FREQUENCIES = 2
+MAX_TIME_FRAMES = 2
 MAX_LABEL_SIZE = 100
 
 inputs = Input(shape=(MAX_TIME_FRAMES, NB_FREQUENCIES))
@@ -45,7 +45,13 @@ model.summary()
 sgd = SGD(nesterov=True)
 model.compile(loss=ctc_loss, metrics=['accuracy'], optimizer=sgd)
 
+ex1 = np.random.random_sample((2,2))
+ex2 = np.random.random_sample((2,2))
+batch = np.array([ex1, ex2])
 
+truth = np.array([[[0, 1]], [[0,1]]])
+
+model.fit(batch, truth, epochs=1)
 
 
 # model.save_weights('models/')
