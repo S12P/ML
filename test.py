bcccf19b2a5e3f48265d3c1bf32d9e4fb8901ae4
Stéphane <stephane.pouget@ens-lyon.fr>
2018-05-01 00:02:47 +0200

    m

--- test.py
+++ test.py
@@ -11,6 +11,9 @@ from keras.optimizers import SGD
 from keras.models import load_model
 import sys
 
+arg = sys.argv
+fichier = str(arg[1])
+
 def ctc_loss_lambda(args):
     y_pred, y_true, input_length, label_length = args
 
@@ -22,9 +25,6 @@ def ctc(y_true, y_pred):
 def clipped_relu(x):
     return keras.activations.relu(x, max_value=20)
 
-arg = sys.argv
-fichier = str(arg[1])
-freq = 161
 
 NB_FREQUENCIES = 161
 
@@ -53,17 +53,32 @@ loss_out = Lambda(ctc_loss_lambda, output_shape=(1, ), name='main_output')([h6, 
 model = keras.models.Model(inputs=[inputs, labels, input_length, label_length], outputs=[loss_out, h6])
 model.summary()
 
-sgd = SGD(nesterov=True)
+if b:
+    model.load_weights('my_model_weights.h5')
 
-model.compile(loss={'main_output': ctc, 'aux_output': lambda x, y: K.constant([0])}, metrics=['accuracy'], optimizer=sgd)
+else:
+    sgd = SGD(nesterov=True)
 
-batch, lab, input_len, lab_len = tt.get_sound_examples('t.wav')
-#out = K.ctc_decode(
-model.predict([batch, lab, input_len, lab_len])
-#[1], input_len)
+    model.compile(loss={'main_output': ctc, 'aux_output': lambda x, y: K.constant([0])}, metrics=['accuracy'], optimizer=sgd)
 
-fichier = sound.spectogram(fichier, freq)
+    batch, lab, input_len, lab_len = tt.get_batch()
 
-output = model.predict(fichier)
+    size_training_set = int(.8 * len(batch))
+    print('The training set is of size {}\n'.format(size_training_set))
+
+    [x_train, x_test] = np.split(batch, [size_training_set])
+    [y_train, y_test] = np.split(lab, [size_training_set])
+    [input_len_train, input_len_test] = np.split(input_len, [size_training_set])
+    [lab_len_train, lab_len_test] = np.split(lab_len, [size_training_set])
 
 
+    model.fit([x_train, y_train, input_len_train, lab_len_train], [y_train, x_train], batch_size=100, epochs=1)
+
+    score = model.evaluate([x_test, y_test, input_len_test, lab_len_test], [y_test, x_test])
+
+    print('The final score is {}'.format(score))
+
+batch, lab, input_len, lab_len = tt.get_sound_examples('examples')
+out = K.ctc_decode(model.predict([batch, lab, input_len, lab_len])[1], input_len)
+
+print(K.eval(out[0]))
